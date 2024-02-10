9c45f3ca0d91d59c44f5160b7b491e330b11cdc0
EunbyeolKo <luvris2@naver.com>
2024-02-10 16:40:31 +0900

    change

---
+++ C#_Winform_StartUp_With_Window_Example/Form1.cs
@@ -0,0 +1,10 @@
+namespace C__Winform_StartUp_With_Window_Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
